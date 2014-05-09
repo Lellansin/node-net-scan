add0931b70bb3f26968ab401e84323b5b52059fb
Lellansin <lellansin@gmail.com>
2014-05-09 23:09:38 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
-var _ = require('..');
+var scan = require('..');
 
-_.scan({
-	host: 'www.google.com.hk',
+console.time('ports scan');
+scan.port({
+	host: 'www.lellansin.com',
 	start: 1,
-	end: 200,
-	timeout: 300
+	end: 100,
+	timeout: 2000,
+	debug: true
 }, function(result) {    
+	console.timeEnd('ports scan');
 	console.log('open ports:' + result);
 });
