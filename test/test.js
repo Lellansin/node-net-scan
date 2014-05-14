f1e00834a63bb322b072d23a0542cb3d1900895c
Lellansin <lellansin@gmail.com>
2014-05-14 17:32:39 +0800

    standard tab to 2 spaces

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@ var scan = require('..');
 
 console.time('ports scan');
 scan.port({
-	host: 'www.lellansin.com',
-	start: 1,
-	end: 100,
-	timeout: 2000,
-	debug: true
+  host: 'www.lellansin.com',
+  start: 1,
+  end: 100,
+  timeout: 2000,
+  debug: true
 }, function(result) {    
-	console.timeEnd('ports scan');
-	console.log('open ports:' + result);
+  console.timeEnd('ports scan');
+  console.log('open ports:' + result);
 });
