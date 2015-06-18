86a6a690b04a0cdd924c6c5bbedfb11960a5b40e
Lellansin <lellansin@gmail.com>
2015-06-18 11:28:30 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@ var scan = require('..');
 
 console.time('ports scan');
 scan.port({
-  host: 'www.lellansin.com',
+  host: 'www.baidu.com',
   start: 1,
   end: 100,
   timeout: 2000,
-  debug: true
-}, function(result) {    
+  queue: 100
+}, function(err, result) {    
   console.timeEnd('ports scan');
   console.log('open ports:' + result);
 });
